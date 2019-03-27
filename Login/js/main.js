dda375bf7adb1aa97e66e90e3e17995f0d82b999
SwaroopMeher <mms.swaroop777@gmail.com>
2019-03-27 10:41:08 +0530

    src refspec master does not match any  src refspec master does not match any

---
+++ Login/js/main.js
@@ -0,0 +1,121 @@
+
+(function ($) {
+    "use strict";
+
+
+    /*==================================================================
+    [ Focus input ]*/
+    $('.input100').each(function(){
+        $(this).on('blur', function(){
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+                $(this).removeClass('has-val');
+            }
+        })
+    })
+
+
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+        });
+    });
+
+    function validate (input) {
+        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+
+    /*==================================================================
+    [ Show pass ]*/
+    var showPass = 0;
+    $('.btn-show-pass').on('click', function(){
+        if(showPass == 0) {
+            $(this).next('input').attr('type','text');
+            $(this).addClass('active');
+            showPass = 1;
+        }
+        else {
+            $(this).next('input').attr('type','password');
+            $(this).removeClass('active');
+            showPass = 0;
+        }
+
+    });
+    
+    $(".slide1").hover(
+      function(){
+       $(".box1").css("margin-right",20);
+  },
+      function(){
+        $(".box1").css("margin-right",50);
+});
+
+$(".slide2").hover(
+  function(){
+   $(".box2").css("margin-right",20);
+},
+  function(){
+    $(".box2").css("margin-right",50);
+
+});
+
+
+$(".slide3").hover(
+  function(){
+   $(".box3").css("margin-right",20);
+},
+  function(){
+    $(".box3").css("margin-right",50);
+});
+
+
+$(".slide4").hover(
+  function(){
+   $(".box4").css("margin-right",20);
+},
+  function(){
+    $(".box4").css("margin-right",50);
+});
+
+})(jQuery);
