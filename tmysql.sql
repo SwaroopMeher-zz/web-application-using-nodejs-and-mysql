f64a76053f83324129a71a7779a76f74e8e5a47b
Swaroop Meher <mms.swaroop777@gmail.com>
2019-04-02 00:13:47 +0530

    added users table

--- tmysql.sql
+++ tmysql.sql
@@ -45,5 +45,23 @@ FLUSH PRIVILEGES;
 /* This is because caching_sha2_password is introduced in MySQL 8.0, 
 but the Node.js version is not implemented yet. You can see this pull request and this issue for more information. 
 Probably a fix will come soon!*/
+delete 
+from song
+where song.id_a=1;
+
+create table users
+(user_name varchar(40) unique,
+password blob
+);
+
+
+INSERT INTO users VALUES ('Hales777', AES_ENCRYPT('swaroop','1404'));
+
+select * from album;
+select * from users;
+select aes_decrypt(password,'1404') as password FROM users;
+truncate table users;
+
+UPDATE musician set first_name="swa",surname="me",address="qece",phone_no="122",id_i="12" where ssn=200;
 
 
